333ec28656da97f1e19b8ae83f7a9fe72428c9a0
Mohamed Saeed <msaeed027@gmail.com>
2020-09-05 23:10:25 +0200

    Add mmm.js

---
+++ mmm.js
@@ -0,0 +1,53 @@
+/*
+    var / let / const
+    function
+    if 
+    for / while
+*/
+
+
+for (let i = 0; i < 3; i++) {
+    let z = "";
+    for (let x = i; x >= 0; x--) { // loop - 2
+         z += "*"  // z = z + "*"
+    }
+    console.log(z)
+}
+
+
+/*
+i=0
+    z = ""
+    x = 0
+    z ="*"
+
+
+i=1
+    z = ""
+    x = 1
+    z = "*"
+    x = 0
+    z = "**"
+
+i=2
+    z = ""
+    x = 2
+    z = "*"
+    x = 1
+    z = "**"
+    x = 
+    z = "***"    
+
+
+------
+*
+**
+***
+
+
+
+
+
+
+
+*/
